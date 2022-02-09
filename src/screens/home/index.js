b08b1f12eff2dc7af3cd8fc396180b9a016918af
niltonc <niltoncesarjr1@gmail.com>
2022-02-09 10:42:03 -0300

    new update: layout finish

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,11 +1,88 @@
 import React from 'react';
-import { Text, View, TextInput, TouchableOpacity, ImageBackground, StyleSheet } from 'react-native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import Item from '../../components/item/index';
+import { Text, View, TextInput, TouchableOpacity, ImageBackground, StyleSheet, ScrollView, FlatList, AsyncStorage } from 'react-native';
+import { MaterialCommunityIcons, Feather  } from '@expo/vector-icons';
+
+
+import itemList from '../../components/item/index';
+
+const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        title: 'Third Item',
+      },
+      {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        title: 'Third Item',
+      },
+    
+  ];
+
+  const Item = ({ title }) => (
+    <View style={{
+        flexDirection: 'row',
+     alignItems: 'center',
+    justifyContent:'center',
+    backgroundColor:'#FFFFFF',
+    borderRadius:15,
+    width: '100%',
+    marginBottom: 8,
+    height: 70
+    }}>
+
+        <TouchableOpacity onPress={() => selectItem()}
+        style={{backgroundColor:'#1221'}}
+        >
+        <Feather name="square" size={20} color="black" />
+				</TouchableOpacity>
+
+                <Text style={[styles.textItem
+                ]}> 
+                {title}
+                </Text>
+
+                <TouchableOpacity onPressOut={() => editItem()} style={{margin:2}}>
+				<Feather name="edit" size={20} color="blue" />
+			</TouchableOpacity>
+
+            <TouchableOpacity onPressOut={() => deleteItem() } style={{margin:2}}>
+            <Feather name="trash" size={20} color="red" />
+			</TouchableOpacity>
+
+    </View>
+  );
+
+  const selectItem = () => {
+      console.log('item selecionado')
+  }
+
+  const deleteItem = () => {
+    console.log('item Deletado')
+}
+
+const editItem = () => {
+    console.log('item Deletado')
+}
+
 
 export default function Home({ navigation }) {
+
+    const renderItem = ({ item }) => (
+        <Item title={item.title} />
+      );
+
     return (
-        
         <View style={styles.container}>
          
             <ImageBackground source={require('../../../assets/background.png')}
@@ -14,31 +91,30 @@ export default function Home({ navigation }) {
                 <View style={styles.titleContainer}>
                     
                     <View style={styles.title}>
-                        <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#FFFFFF' }}>Lista de Compras</Text>
+                        <Text style={styles.textTitle}>Lista de Compras</Text>
                     </View>
                     
                     <View style={styles.itemCounter}>
-                        <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#FFFFFF' }}>1/2</Text>
+                        <Text style={styles.textTitle}>1/2</Text>
                     </View>
                 </View>
 
                {/* FLATLIST */}
                 <View style={styles.flatlistContainer}>
-                
-                   {/* <Item
-								//isCompleted={row.item.isCompleted}
-								//textValue={row.item.textValue}
-								//id={row.item.id}
-								deleteTodo={this.deleteTodo}
-								completeTodo={this.completeTodo}
-								inCompleteTodo={this.inCompleteTodo}
-							/> */}
+                <ScrollView> 
+
+                <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+          
+                </ScrollView>
                 </View>
 
                 {/* ADICIONA O ITEM A LISTA  */}
 
-               
-                <View style={styles.insertItemContainer}>
+               <View style={styles.insertItemContainer}>
 
                     {/* view text input */}
                     <View style={styles.textInputContainer}>
@@ -85,18 +161,24 @@ const styles = StyleSheet.create({
         backgroundColor: 'transparent', 
         width: 240
     },
+    textTitle:{
+        fontSize: 20, 
+        fontWeight: 'bold', 
+        color: '#FFFFFF'
+    },
     itemCounter:{
         alignSelf: 'center', 
         justifyContent: 'center', 
         width: 100, 
         backgroundColor: 'transparent', 
         alignItems: 'center' 
-    },
+    }, 
     flatlistContainer:{
         backgroundColor: '#1224', 
         width: '100%', 
-        height: 450, 
-        alignItems: 'center', 
+        height: '60%', 
+        alignItems: 'center',
+        justifyContent:'space-between', 
         margin:10
     },
     insertItemContainer:{
@@ -134,5 +216,13 @@ const styles = StyleSheet.create({
         alignItems: 'center', 
         justifyContent: 'center', 
         borderRadius: 10,
-    }
+    },
+    textItem:{
+        color: '#e85128',
+		fontSize: 18,
+        backgroundColor:'#1221',
+        width: 190,
+        justifyContent:'center',
+        margin: 8
+    },
 })
